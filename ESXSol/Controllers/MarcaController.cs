be068d3ecefb0c017d58d72d044fddbbcdbb314c
rafael sanchez port <rafysanchez@hotmail.com>
2019-10-01 20:12:53 -0300

    marca controller

--- ESXSol/Controllers/MarcaController.cs
+++ ESXSol/Controllers/MarcaController.cs
@@ -1,4 +1,6 @@
-﻿using ESXSol.Repositories;
+﻿using ESXSol.Models;
+using ESXSol.Repositories;
+using ESXSol.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -15,7 +17,75 @@ namespace ESXSol.Controllers
             _repository = repository;
         }
 
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<MarcaViewModel>> Get()
+        {
+            return Ok(_repository.Get());
 
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<MarcaViewModel> Get(int id)
+        {
+            var result = _repository.GetById(id);
+
+            return Ok(result);
+
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ResultViewModel Post([FromBody] Marca value)
+        {
+            var marca = new Marca();
+            marca.Nome = value.Nome;
+
+
+            _repository.Save(marca);
+
+            return new ResultViewModel
+            {
+                Success = true,
+                Message = "Produto cadastrado com sucesso!",
+                Data = marca
+            };
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public ResultViewModel Put(int id, [FromBody]Marca value)
+        {
+            var marca = _repository.GetById(value.MarcaId);
+
+            marca.Nome = value.Nome;
+            _repository.Update(marca);
+
+            return new ResultViewModel
+            {
+                Success = true,
+                Message = "Produto alterado com sucesso!",
+                Data = marca
+            };
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public ResultViewModel Delete(int id)
+        {
+            var marca = _repository.GetById(id);
+
+            _repository.Delete(id);
+
+            return new ResultViewModel
+            {
+                Success = true,
+                Message = "Produto excluido com sucesso!",
+                Data = marca
+            };
+        }
 
 
     }
--- ESXSol/Repositories/MarcaRepository.cs
+++ ESXSol/Repositories/MarcaRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ESXSol.Data;
+using ESXSol.Models;
+using ESXSol.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +11,44 @@ namespace ESXSol.Repositories
 {
     public class MarcaRepository
     {
+        private readonly ESXDataContext _context;
 
+        public MarcaRepository(ESXDataContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<MarcaViewModel> Get()
+        {
+            return _context
+                .Marca
+                .Select(x => new MarcaViewModel
+                {
+                    MarcaId = x.MarcaId,
+                    Nome = x.Nome
+                  
+                })
+                .ToList();
+        }
+        public Marca GetById(int id)
+        {
+            return _context.Marca.Find(id);
+        }
+        public void Save(Marca marca)
+        {
+            _context.Marca.Add(marca);
+            _context.SaveChanges();
+        }
+        public void Update(Marca marca)
+        {
+            _context.Entry<Marca>(marca).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int Id)
+        {
+            _context.Marca.Remove(_context.Marca.Where(x=>x.MarcaId==Id).First());
+            _context.SaveChanges();
+        }
     }
 }
